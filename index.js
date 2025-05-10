684639dc954e498fdfab562d8b1b895517e3d62d
jmoreno1523 <julian.moreno01@unicatolica.edu.co>
2025-05-09 22:26:48 -0500

    se modifico el index.js

--- index.js
+++ index.js
@@ -2,19 +2,25 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-
+const path = require('path'); // Añade el módulo path si vas a servir archivos
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+app.get('/', (req, res) => { 
+  res.send('¡Bienvenido a mi API!');
+  
+});
+
+app.use('/api/auditorios', require('./routes/auditorios'));
+app.use('/api/chatgpt', require('./routes/chatgpt'));
+
+// --- CONEXIÓN A MONGODB ---
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('✅ MongoDB conectado'))
   .catch(err => console.error('❌ Error de conexión:', err));
 
-app.use('/api/auditorios', require('./routes/auditorios'));
-
+// --- INICIO DEL SERVIDOR ---
 app.listen(5000, () => console.log('🚀 Servidor corriendo en puerto 5000'));
-
-app.use('/api/chatgpt', require('./routes/chatgpt'));
-
